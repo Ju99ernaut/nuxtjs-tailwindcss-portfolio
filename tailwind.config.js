bb74de8f5de639f1bfa6a1a01f9f4a6fa7e156b5
Ju99ernaut <48953676+Ju99ernaut@users.noreply.github.com>
2022-04-23 20:56:32 +0200

    Update theme

--- tailwind.config.js
+++ tailwind.config.js
@@ -16,9 +16,9 @@ module.exports = {
         "secondary-light": "#FFFFFF",
         "ternary-light": "#f6f7f8",
 
-        "primary-dark": "#0D2438",
-        "secondary-dark": "#102D44",
-        "ternary-dark": "#1E3851",
+        "primary-dark": "#0f0f16",
+        "secondary-dark": "#23222b",
+        "ternary-dark": "#161920",
       },
       container: {
         padding: {
